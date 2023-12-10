25e6b87bb94b82ad84264123d1c0c08de938a401
Арман <@ARMAN>
2023-12-10 23:30:36 +0600

    Добавьте файлы проекта.

---
+++ dom15(1)/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        // Получаем тип класса Console
+        Type consoleType = typeof(Console);
+
+        // Получаем массив методов класса Console
+        MethodInfo[] methods = consoleType.GetMethods();
+
+        // Выводим на экран список методов
+        Console.WriteLine("Методы класса Console:");
+        foreach (MethodInfo method in methods)
+        {
+            Console.WriteLine(method.Name);
+        }
+    }
+}
